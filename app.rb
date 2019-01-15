fb4b38c55252d432e986f58eeb4aec331be1bf4d
Saint-Jean Germila <germila@noukod.com>
2019-01-15 17:15:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,36 +3,52 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 get '/reversename/:name' do
-@reversename = @params[:name].reverse
-
+@reversename =  params[:name].reverse
+"#{@reversename}"
 end
 
 get '/square/:number' do
-  @square= @params[:number].to_i*@params[:number].to_i
-  "#{square}"
+  @square=  params[:number].to_i*params[:number].to_i
+  "#{@square}"
 end
 
 get '/say/:number/:phrase' do
  n.times do
-    @say=@params[:number].to_i@params[:phrase]
+    @say=@params[:number].to_i
+    @say=@params[:phrase]
+    @result=""
+    @number.times do
+      @result+="#{:phrase}"
+    end
+    @result
   end
 end
 
 get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @say=@params[:word1].@params[:word2].@params[:word3].@params[:word4].@prams[:word5]
+    @say=@params[:word1]" "@params[:word2]" "@params[:word3]" "@params[:word4]" "@prams[:word5]
+    "#{@say}"
 end
 
 end
 
-get '/:operation/:number1/:number2' do
-  @sum =@params[:number1].to_i+@params[:number2].to_i
-  "#{sum}"
+get "/:operation/:number1/:number2" do
+ @operation=@params[:operation]
+ @operand1=@params[:number1].to_i
+ @operand2=@params[:number2].to_i
+ @result=1
+ case @operation
+   when "add"
+     @result=@operand1+@operand2
+   when "subtract"
+     @result=@operand1-@operand2
+   when "multiply"
+     @result=@operand1*@operand2
+   when "divide"
+     @result=@operand1/@operand2
+ end
 
-  @product =@params[:number1].to_i*@params[:number2].to_i
-  "#{product}"
+ "#{@result}"
 
-  @div =@params[:number1].to_i/@params[:number2].to_i
-  "#{div}"
 end
 
 end
